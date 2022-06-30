6bc31b96e852c2dc9022053304b369e5f8618ed4
David Watson <david@neonquill.com>
2022-06-29 21:32:47 -0400

    Pull in cargo options

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 use clap::Parser;
+use probe_rs_cli_util::common_options::CargoOptions;
 
 #[derive(Parser, Debug)]
 #[clap(version, about)]
 struct Args {
     #[clap(short, long, value_parser)]
     name: String,
+
+    #[clap(flatten)]
+    cargo_options: CargoOptions,
 }
 
 fn main() {
     let args = Args::parse();
 
     println!("Hello, {}!", args.name);
+    println!("Cargo: {:?}", args.cargo_options);
 }
