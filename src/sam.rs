17a9b48d141170dbedb039c64f396987610f3bfc
David Watson <david@neonquill.com>
2022-07-01 06:48:40 -0400

    Finish chip erase sequence (untested)

--- src/sam.rs
+++ src/sam.rs
@@ -41,17 +41,25 @@ impl Atsaml10 {
     const DSU_ADDR: u32 = 0x41002100;
     const DSU_STATUSA_ADDR: u32 = Self::DSU_ADDR + 0x1;
     const DSU_STATUSB_ADDR: u32 = Self::DSU_ADDR + 0x2;
+    const DSU_BCC0_ADDR: u32 = Self::DSU_ADDR + 0x20;
+    const DSU_BCC1_ADDR: u32 = Self::DSU_ADDR + 0x24;
     // XXX Handle registers better.
     const CRSTEXT_BIT: u8 = 1 << 1;
     const BCCD1_BIT: u8 = 1 << 7;
     // Boot Interactive Mode commands (14.4.5.9).
     // Enter Interactive Mode.
-    const _CMD_INIT: u32 = 0x444247_55;
+    const CMD_INIT: u32 = 0x444247_55;
     // Exit Interactive Mode.
     const _CMD_EXIT: u32 = 0x444247_AA;
     // ChipErease for SAM L10.
-    const _CMD_CHIPERASE: u32 = 0x444247_E3;
+    const CMD_CHIPERASE: u32 = 0x444247_E3;
     // Boot Interactive Mode Status (14.4.5.10).
+    // Debugger start communication.
+    const SIG_COMM: u32 = 0xEC0000_20;
+    // Dubber command success.
+    const SIG_CMD_SUCCESS: u32 = 0xEC0000_21;
+    // Valid command.
+    const SIG_CMD_VALID: u32 = 0xEC0000_24;
     const _NVMCTRL_STATUS_READY: u8 = 1 << 2;
 
     pub fn new() -> Self {
@@ -117,6 +125,61 @@ impl Atsaml10 {
         // XXX Still need to actually run the erase command.
 
         log::warn!("XXXa4");
+
+        // Request Boot ROM Interactive mode entry (14.4.5.1.1).
+        memory.write_word_32((Self::DSU_BCC0_ADDR).into(), Self::CMD_INIT)?;
+
+        log::warn!("XXXa5");
+
+        // Check for SIG_COMM status in DSU.BCC1.
+        let status = memory.read_word_32((Self::DSU_BCC1_ADDR).into())?;
+        // Possibly I need to wait for the bit to be set?
+        if status != Self::SIG_COMM {
+            log::warn!("XXX status wrong: {:x}", status);
+            return Err(anyhow!("Failed to enter Boot ROM interactive mode."));
+        }
+
+        log::warn!("XXXa6");
+
+        // Issue the Chip Erase command (14.4.5.4.1).
+        memory
+            .write_word_32((Self::DSU_BCC0_ADDR).into(), Self::CMD_CHIPERASE)?;
+
+        // Check to see if the command was valid.
+        let status = memory.read_word_32((Self::DSU_BCC1_ADDR).into())?;
+        if status != Self::SIG_CMD_VALID {
+            log::warn!("XXX status wrong: {:x}", status);
+            return Err(anyhow!("Chip Erase failed due to invalid command"));
+        }
+
+        log::warn!("XXXa7");
+
+        // Poll for status update.
+        let mut status = 0;
+        for i in 0..20 {
+            status = memory.read_word_32((Self::DSU_BCC1_ADDR).into())?;
+            if status != Self::SIG_CMD_VALID && status != 0 {
+                // XXX Change this to trace.
+                log::warn!("Received status update after {} cycles", i);
+                break;
+            }
+            // No status update, wait for a while before trying again.
+            thread::sleep(Duration::from_secs(1));
+        }
+
+        log::warn!("XXXa8");
+
+        // Make sure we were successful.
+        if status != Self::SIG_CMD_SUCCESS {
+            // XXX is warn the right message?
+            log::warn!("XXX Chip Erase failed!");
+            // XXX reset to park?
+        } else {
+            // XXX warn?
+            log::warn!("XXX Chip Erase succeeded");
+        }
+
+        log::warn!("XXXa9");
 
         Ok(())
     }
